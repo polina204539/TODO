f8b503e3c10bb30cfc0aa8fa61684e1777822433
Polina <PolinaSaparova@gmail.com>
2023-10-13 20:53:43 +0300

    feat(views): проверка принадлежности к группе

--- todolist/todo/views.py
+++ todolist/todo/views.py
@@ -1,8 +1,17 @@
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
 
-@login_required
+
+def group_required(*group_names):
+    """Requires user membership in at least one of the groups passed in."""
+    def in_groups(u):
+        if u.is_authenticated:
+           if bool(u.groups.filter(name__in=group_names)) | u.is_superuser:
+               return True
+        return False
+    return user_passes_test(in_groups)
+
+@group_required("admins", "seller")
 def index(request):
     return HttpResponse("Hello METANIT.COM")
-
-    
