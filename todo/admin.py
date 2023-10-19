977c0776f142bd0d82c58d2a735fff322efa670f
Polina <PolinaSaparova@gmail.com>
2023-10-19 17:40:42 +0300

    feat: добавление статуса

--- todo/admin.py
+++ todo/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Todolist
+from .models import Todolist, Status
 
 admin.site.register(Todolist)
+admin.site.register(Status)
--- todo/models.py
+++ todo/models.py
@@ -4,7 +4,7 @@ class Todolist(models.Model):
     """Список задач"""
     id = models.AutoField(primary_key=True)
     description = models.TextField("Описание")
-    status = models.BooleanField('Выполнено')
+    status = models.ForeignKey('Status', on_delete=models.PROTECT)
 
     def __str__(self):
         return self.description
@@ -12,3 +12,15 @@ class Todolist(models.Model):
     class Meta:
         verbose_name = "Задача"
         verbose_name_plural = "Задачи"
+
+class Status(models.Model):
+    """Статус"""
+    id = models.AutoField(primary_key=True)
+    name = models.TextField("Название")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Статус"
+        verbose_name_plural = "Статусы"  
